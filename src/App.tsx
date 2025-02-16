9b2c56a7a02722a0a3487cbd2b491d7769b06961
santonssss <sarvarkalmuratov370@gmail.com>
2025-02-17 00:41:16 +0500

    change logic route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Login from "./Pages/Login";
 import Chat from "./Pages/Chat";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const instanceId = localStorage.getItem("idInstance");
+    const apiTokenInstance = localStorage.getItem("apiTokenInstance");
+
+    if (!instanceId || !apiTokenInstance) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
